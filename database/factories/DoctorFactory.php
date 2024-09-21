a2e45d0c98b811bb2b9c30be61927cc1d5d3dfb9
Buraito-stack <bryantedyluke@gmail.com>
2024-09-22 02:01:18 +0700

    Minor fix DoctorFactory.php and modify routes in web.php

--- database/factories/DoctorFactory.php
+++ database/factories/DoctorFactory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Specialist;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory
  */
 class DoctorFactory extends Factory
 {
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,15 @@ use App\Http\Controllers\JadwalPasienController;
 use App\Http\Controllers\DasboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('admin') 
+    ->group(function() {
+        Route::controller(DasboardController::class)
+            ->prefix('dashboard')
+            ->name('dasboard')
+            ->group(function() {
+                Route::get('/', 'index')->name('index');
+            });
+    });
 
 Route::apiResource('/list/doctors', ListDokterController::class);
 Route::apiResource('/jadwal/pasien', JadwalPasienController::class);
